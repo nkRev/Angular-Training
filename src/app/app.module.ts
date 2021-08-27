39e162eaa824b38f3bf0676dc8687ad85eb1c701
nkRev <nicky.kayyarath@revature.net>
2021-08-26 23:43:43 -0400

    learned about components and build a mock website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainContentComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
